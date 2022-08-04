865c8caf4c532f89eb3e07adcbb9b9da24887070
Budi Sugianto <budisugianto777@gmail.com>
2022-08-04 00:14:23 -0400

    trying

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,6 +3,7 @@ require 'capybara/cucumber'
 require 'cucumber'
 require 'pry'
 require 'rspec'
+require 'selenium-webdriver'
 require 'webdrivers' unless File.exist?('/usr/bin/chromedriver')
 
 chrome_options = { args: %w[ --no-sandbox --enable-automation --disable-gpu ]}
