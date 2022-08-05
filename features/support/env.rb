e408a0d924d81eff702d891715d3c439085468ea
Budi Sugianto <budisugianto777@gmail.com>
2022-08-04 21:33:47 -0400

    trying

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,11 +6,6 @@ require 'rspec'
 require 'selenium-webdriver'
 require 'webdrivers'
 
-options = Selenium::WebDriver::Chrome::Options.new
-options.add_argument('--headless') if ENV['HEADLESS']
-options.add_argument('--disable-dev-shm-usage')
-options.add_argument('--disable-gpu')
-
 Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome, timeout: 30, options: options)
 end
