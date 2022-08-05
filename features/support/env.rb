325e1541d5c52b346df4dadb486afd0048e99886
Budi Sugianto <budisugianto777@gmail.com>
2022-08-04 22:58:12 -0400

    Fix the github-action config and setup

--- features/support/env.rb
+++ features/support/env.rb
@@ -7,9 +7,9 @@ require 'selenium-webdriver'
 require 'webdrivers'
 
 options = Selenium::WebDriver::Chrome::Options.new
-options.add_arguments('--headless') if ENV['HEADLESS']
-options.add_arguments('--disable-dev-shm-usage')
-options.add_arguments('--disable-gpu')
+options.add_argument('--headless') if ENV['HEADLESS']
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--disable-gpu')
 
 Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome, timeout: 30, options: options)
