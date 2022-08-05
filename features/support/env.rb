6237899f949d90e51367489ba1a3bafb0c4e1e88
Budi Sugianto <budisugianto777@gmail.com>
2022-08-04 21:30:34 -0400

    clean up

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,17 +4,12 @@ require 'cucumber'
 require 'pry'
 require 'rspec'
 require 'selenium-webdriver'
-require 'webdrivers/chromedriver'
-
-webdriver_local_path = Webdrivers::Chromedriver.update
-webdriver_directory = webdriver_local_path.split('/')[0..-2].join('/')
-ENV['PATH'] = "#{webdriver_directory}:" + ENV['PATH']
+require 'webdrivers'
 
 options = Selenium::WebDriver::Chrome::Options.new
 options.add_argument('--headless') if ENV['HEADLESS']
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-gpu')
-options.add_argument('--remote-debugging-port=9222')
 
 Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome, timeout: 30, options: options)
